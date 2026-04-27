79d578416418af13d4b3bd65ec4f8a47997d805f
Laura <lauracastrotech@gmail.com>
2026-04-27 13:41:52 -0400

    adds semantic html elements as placeholders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      
+      <main className="font-display bg-slate center">HERE GOES THE PARALAX SCROLL CONTENT</main>
+      <footer className="font-body bg-olive justify">HERE GOES CONTACT FORM, SOCIAL MEDIA ICONS, SITE MAP, CONTACT INFORMATION</footer>
     </div>
   );
 }
